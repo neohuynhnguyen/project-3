12dc85a89b2c348f139d68f7707faa0d67dcdb66
neohuynhnguyen <huynhnguyen09021994@gmail.com>
2022-05-12 08:44:45 +0700

    Initial commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=HuynhNV2
+export POSTGRES_HOST=localhost
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::udaconnect
+export AWS_REGION=us-west-2
+export AWS_PROFILE=test
+export JWT_SECRET=testing
+export URL=http://localhost:8100
